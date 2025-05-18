504b640b113066a7ef860661b00352414c222df3
AGNESH KUMAR <agneshkumar257@gmail.com>
2025-05-18 09:10:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+var typed = new Typed(".ap", {
+    strings: ["Frontend Developer", "Web designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
